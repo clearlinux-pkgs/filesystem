1cb20cb7ebef8bf320744d5787e33953239b444f
Ikey Doherty <michael.i.doherty@intel.com>
2015-06-24 13:51:52 +0100

    Ensure PS1 is working on xfce terminal
    Signed-off-by: Ikey Doherty <michael.i.doherty@intel.com>

--- 50-prompt.sh
+++ 50-prompt.sh
@@ -3,8 +3,16 @@ if [ "$UID" = "0" ]; then
     endchar="#"
 fi
 
-FG="\[\e[38;2;255;142;58m\]"
-BG="\[\e[38;2;0;174;255m\]"
+# Blue
+BG="\[\e[38;5;39m\]"
+# RGBA index:
+#BG="\[\e[38;2;255;142;58m\]"
+
+# Orange
+FG="\[\e[38;5;208m\]"
+# RGBA index:
+#FG="\[\e[38;2;0;174;255m\]"
+
 export PS1="$BG\u\[\e[0m\]@$FG\H ${BG}\w ${BG}$endchar \[\e[0;0m\]"
 if [ "${TERM:0:5}" = "xterm" ]; then
   export PS1="\[\e]2;\u@\H :: \w\a\]$PS1"
