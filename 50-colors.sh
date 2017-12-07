f106375cadd9514253d4bb88e1885b603d6dbfac
Auke Kok <auke-jan.h.kok@intel.com>
2017-12-07 12:53:08 -0800

    Add LS_COLORS usage to 50-colors.sh as default fallback.

--- 50-colors.sh
+++ 50-colors.sh
@@ -2,10 +2,10 @@
 alias ls='ls --color=auto'
 if [ -f "$HOME/.dircolors" ]; then
   eval `dircolors -b "$HOME/.dircolors"`
-else
-  if [ -f "/etc/dircolors" ]; then
-    eval `dircolors -b "/etc/dircolors"`
-  fi
+elif [ -f "/etc/dircolors" ]; then
+  eval `dircolors -b "/etc/dircolors"`
+elif [ -f "/usr/share/defaults/etc/LS_COLORS" ]; then
+  eval $( dircolors -b /usr/share/defaults/etc/LS_COLORS )
 fi
 
 # GCC diagnostics/colors
