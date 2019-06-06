ab854b4bc4fa15aea7f0a129a7287fc16336a0bd
Lucius Hu <orctarorga@gmail.com>
2019-06-06 17:28:07 -0500

    Update prompt for bash and zsh
    This is a part of a bigger plan that bourne compatible shells shall
    have a same set of default profiles.
    
    This patch shall be applied on `50-prompt.sh` in
    `clearlinux-pkgs/filesystem`, it includes the following:
    
    1. It would be set the prompt correctly for both bash and zsh.
       Currently the prompt for bash is defined here, but the prompt for
       zsh is set in `/usr/share/defaults/. Moreover, their prompts are
       not identical.
    2. The color for bash prompt is not set correctly, it's fixed here.
    3. The end character for zsh is not set correctly, as well as the
       color, which are both fixed now.
    4. When the terminal is xterm*, this file sets the title for both
       bash and zsh. Currently the prompt for bash is defined here, but
       it's not set for zsh.
    5. Refactored variable names to increase readability.
    
    I also wrap the code in functions so and made the variable local.
    Also, I used the hooks in bash and zsh.
    
    This patch will requires the changes in the following:
    
    1. `profile.x86_64` and `profile.i386` in `clearlinux-pkgs/filesystem`
       shall not set anything related to prompt.
    2. `0001-stateless-configuration.patch` in `clearlinux-pkgs/zsh`
       shall be patched so that `zshrc` won't set the prompt.
    
    This has been tested locally for sh (aliased to bash), bash, and zsh.

--- 50-prompt.sh
+++ 50-prompt.sh
@@ -1,21 +1,64 @@
-endchar="\$"
-if [ "$UID" = "0" ]; then
-    endchar="#"
+# Set prompt and title (for interactive shells only)
+if [ "$(expr $- : '.*i')" -ne 0 ]; then
+
+  # this works for sh and bash
+  if [ -z "$ZSH_VERSION" ]; then
+    __stateless_prompt() {
+      local EXIT="$?"                     # exit code of last command
+      local BLUE="\[\e[38;5;39m\]"
+      local RED="\[\e[31m\]"
+      local ORANGE="\[\e[38;5;208m\]"
+      local WHITE="\[\e[0m\]"
+      # endchar and username
+      local endchar="\$${WHITE}"          # $ for non-root users
+      local username="${BLUE}\u${WHITE}"  # blue(39) for non-root
+      if [ "$UID" = "0" ]; then
+        endchar="#${WHITE}"               # # for root user
+        username="${RED}\u${WHITE}"       # red for root
+      fi
+      if [ "$EXIT" -eq 0 ]; then
+        endchar="${WHITE}$endchar"        # White enchar as default
+      else
+        endchar="${RED}$endchar"          # Red endchar for error
+      fi
+      # hostname in orange
+      local host="${ORANGE}\H${WHITE}"
+      # current directory in blue(39)
+      local dir="${BLUE}\w${WHITE}"
+      # set prompt
+      export PS1="${username}@${host} ${dir} ${endchar} "
+      # set window title for xterm
+      if [ "${TERM:0:5}" = "xterm" ]; then
+        export PS1="\[\e]2;\u@\H :: \w\a\]$PS1"
+      fi
+    }
+
+    export PROMPT_COMMAND=__stateless_prompt
+
+  else
+    # this works for zsh
+    __stateless_prompt() {    # set prompt
+      # endchar
+      # use red if last command has non-zero exit
+      # use # for root and $ for non-root users
+      local root_endch="%(?.#.%F{red}#%f)"
+      local other_endch="%(?.$.%F{red}$%f)"
+      local endchar="%(#.${root_endch}.${other_endch})"
+      # use red for root and blue(39) for non-root users
+      local username="%F{%(#.red.39)}%n%f"
+      # hostname in orange
+      local host="%F{208}%m%f"
+      # current directory in blue(39)
+      local dir="%F{39}%~%f"
+      export PS1="${username}@${host} ${dir} ${endchar} "
+    }
+    __stateless_title () {    # for xterm, set window title
+      if [ "${TERM:0:5}" = "xterm" ]; then
+        print -Pn "\e]2;%n@%m :: %~\a"
+      fi
+    }
+    autoload -Uz add-zsh-hook
+    add-zsh-hook preexec __stateless_prompt
+    add-zsh-hook precmd __stateless_title
+  fi
 fi
-
-# Blue
-BG="\[\e[38;5;39m\]"
-# RGBA index:
-#BG="\[\e[38;2;255;142;58m\]"
-
-# Orange
-FG="\[\e[38;5;208m\]"
-# RGBA index:
-#FG="\[\e[38;2;0;174;255m\]"
-
-export PS1="$BG\u\[\e[0m\]@$FG\H ${BG}\w ${BG}$endchar \[\e[0;0m\]"
-if [ "${TERM:0:5}" = "xterm" ]; then
-  export PS1="\[\e]2;\u@\H :: \w\a\]$PS1"
-fi
-
-shopt -s checkwinsize 
