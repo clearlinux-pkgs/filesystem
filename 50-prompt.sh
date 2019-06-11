8ca1bb32c468f19305309d0422c4ccce4b127264
Lucius Hu <orctarorga@gmail.com>
2019-06-11 12:13:46 -0500

    honors PS1 from user settings
    An updated version, which doesn't rely on hooks except for setting
    window titles for zsh.
    
    For bash, without using hooks, I have to use escape sequences which greatly
    reduces the readability.

--- 50-prompt.sh
+++ 50-prompt.sh
@@ -3,62 +3,67 @@ if [ "$(expr $- : '.*i')" -ne 0 ]; then
 
   # this works for sh and bash
   if [ -z "$ZSH_VERSION" ]; then
-    __stateless_prompt() {
-      local EXIT="$?"                     # exit code of last command
-      local BLUE="\[\e[38;5;39m\]"
-      local RED="\[\e[31m\]"
-      local ORANGE="\[\e[38;5;208m\]"
-      local WHITE="\[\e[0m\]"
-      # endchar and username
-      local endchar="\$${WHITE}"          # $ for non-root users
-      local username="${BLUE}\u${WHITE}"  # blue(39) for non-root
-      if [ "$UID" = "0" ]; then
-        endchar="#${WHITE}"               # # for root user
-        username="${RED}\u${WHITE}"       # red for root
-      fi
-      if [ "$EXIT" -eq 0 ]; then
-        endchar="${WHITE}$endchar"        # White enchar as default
-      else
-        endchar="${RED}$endchar"          # Red endchar for error
-      fi
+      # first get exit code of last command, and set colors
+      PS1="\$(\
+      EXIT=\"\$?\" ; \
+      BLUE=\"\[\e[38;5;39m\]\" ; \
+      RED=\"\[\e[31m\]\" ; \
+      ORANGE=\"\[\e[38;5;208m\]\" ; \
+      WHITE=\"\[\e[0m\]\" ; "
+      # endchar
+      #   use # for root and $ for non-root users
+      #   use white for default color, and red if last exit code is non-zero
+      # username
+      #   use red for root blue(39) for non-root users
+      PS1+="\
+      endchar=\"\\$\${WHITE}\" ; \
+      username=\"\${BLUE}\u\${WHITE}\" ; \
+      if [ \"\$UID\" = \"0\" ]; then
+          username=\"\${RED}\u\${WHITE}\" ; \
+      fi ; \
+      if [ \"\$EXIT\" -eq 0 ]; then
+          endchar=\"\${WHITE}\$endchar\" ; \
+      else \
+          endchar=\"\${RED}\$endchar\"
+      fi ; "
       # hostname in orange
-      local host="${ORANGE}\H${WHITE}"
+      PS1+="\
+      host=\"\${ORANGE}\H\${WHITE}\" ; "
       # current directory in blue(39)
-      local dir="${BLUE}\w${WHITE}"
-      # set prompt
-      export PS1="${username}@${host} ${dir} ${endchar} "
-      # set window title for xterm
+      PS1+="\
+      dir=\"\${BLUE}\w\${WHITE}\" ; "
+      # set prompt, and additionally set window title for xterm
       if [ "${TERM:0:5}" = "xterm" ]; then
-        export PS1="\[\e]2;\u@\H :: \w\a\]$PS1"
+          PS1+="echo \"\[\e]2;\u@\H :: \w\a\]\${username}@\${host}\${dir} \${endchar} \" )"
+      else
+          PS1+="echo \"\${username}@\${host} \${dir} \${endchar} \" )"
       fi
-    }
-
-    export PROMPT_COMMAND=__stateless_prompt
 
   else
     # this works for zsh
-    __stateless_prompt() {    # set prompt
       # endchar
       # use red if last command has non-zero exit
       # use # for root and $ for non-root users
-      local root_endch="%(?.#.%F{red}#%f)"
-      local other_endch="%(?.$.%F{red}$%f)"
-      local endchar="%(#.${root_endch}.${other_endch})"
+      local _root_endch="%(?.#.%F{red}#%f)"
+      local _other_endch="%(?.$.%F{red}$%f)"
+      local _endchar="%(#.${_root_endch}.${_other_endch})"
       # use red for root and blue(39) for non-root users
-      local username="%F{%(#.red.39)}%n%f"
+      local _username="%F{%(#.red.39)}%n%f"
       # hostname in orange
-      local host="%F{208}%m%f"
+      local _host="%F{208}%m%f"
       # current directory in blue(39)
-      local dir="%F{39}%~%f"
-      export PS1="${username}@${host} ${dir} ${endchar} "
-    }
-    __stateless_title () {    # for xterm, set window title
-      if [ "${TERM:0:5}" = "xterm" ]; then
-        print -Pn "\e]2;%n@%m :: %~\a"
-      fi
-    }
-    autoload -Uz add-zsh-hook
-    add-zsh-hook preexec __stateless_prompt
-    add-zsh-hook precmd __stateless_title
+      local _dir="%F{39}%~%f"
+      # set prompt
+      PS1="${_username}@${_host} ${_dir} ${_endchar} "
+      # additionally set window title for xterm
+      __stateless_title () {    # for xterm, set window title
+        if [ "${TERM:0:5}" = "xterm" ]; then
+            print -Pn "\e]2;%n\@%m :: %~\a"
+        fi
+      }
+      __stateless_title
+      autoload -Uz add-zsh-hook
+      add-zsh-hook chpwd __stateless_title
   fi
+  export PS1
 fi
